ac91ca6ab3385abf9b0092670a9fb586b754ce9e
Cinthya Cabanzo <cinthya.cabanzo@gmail.com>
2020-09-11 20:15:42 -0500

    se incluye manejo errores llamada API

--- news/src/actions/newsActions.js
+++ news/src/actions/newsActions.js
@@ -3,40 +3,50 @@ import { TRAER_TODAS, TRAER_TODAS_CATEGORIA, TRAER_BUSQUEDA } from '../types/new
 export const traerTodas = ({ date }) => async (dispatch) => {
     const url = `https://api.canillitapp.com/latest/${date}`;
     // const response = await axios.get(`${this.url}${this.state.endpoint}${this.state.date}`);
-    const response = await axios.get(`${url}`);
+    try {
+        const response = await axios.get(`${url}`);
+        dispatch({
+            type: TRAER_TODAS,
+            payload: response.data
+        })
+        console.log("news: ", response);
 
-    console.log("news: ", response);
+    } catch (error) {
+        console.error("error:", error.message);
+    }
 
-    dispatch({
-        type: TRAER_TODAS,
-        payload: response.data
-    })
 }
 
 export const traerTodasCategoria = ({ category }) => async (dispatch) => {
     const url = `https://api.canillitapp.com/news/category/${category}`;
     // const response = await axios.get(`${this.url}${this.state.endpoint}${this.state.date}`);
-    const response = await axios.get(url);
+    try {
+        const response = await axios.get(url);
+        dispatch({
+            type: TRAER_TODAS_CATEGORIA,
+            payload: response.data
+        })
+        console.log("url: ", url);
+        console.log("news: ", response);
+    } catch (error) {
+        console.error("error:", error.message);
+    }
 
-    console.log("url: ", url);
-    console.log("news: ", response);
-
-    dispatch({
-        type: TRAER_TODAS_CATEGORIA,
-        payload: response.data
-    })
 }
 
 export const traerBusqueda = ({ search }) => async (dispatch) => {
     const url = 'https://api.canillitapp.com/${search}';
     // const response = await axios.get(`${this.url}${this.state.endpoint}${this.state.date}`);
-    const response = await axios.get(`${url}`);
+    try {
+        const response = await axios.get(`${url}`);
+        console.log("url: ", url);
 
-    console.log("url: ", url);
-    console.log("news: ", response);
+        dispatch({
+            type: TRAER_BUSQUEDA,
+            payload: response.data
+        })
 
-    dispatch({
-        type: TRAER_BUSQUEDA,
-        payload: response.data
-    })
+    } catch (error) {
+        console.error("error:", error.message);
+    }
 }
